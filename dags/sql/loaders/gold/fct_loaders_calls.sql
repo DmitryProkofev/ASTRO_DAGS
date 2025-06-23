8f5d2e3bd33ae5c427022568c46f31115f8a790c
d.prokofev <d.prokofev@pegas-agro.ru>
2025-06-23 15:50:19 +0400

    + разработал таблицу фактов для погрузчиков

--- dags/sql/loaders/gold/fct_loaders_calls.sql
+++ dags/sql/loaders/gold/fct_loaders_calls.sql
@@ -1,5 +1,7 @@
 #
 
+--create table gold_layer.fct_loaders_calls engine=MergeTree order by datetime_key_open as
+INSERT INTO gold_layer.fct_loaders_calls
 select
 	lc.id, 
 	dc.DateKey as datetime_key_open,
@@ -7,7 +9,15 @@ select
 	dpo.id_srgt as customer_id,
 	dlr.srgt_id as reason_id,
 	dlw.srgt_id as workshop_id,
-	dpo2.id_srgt as loader_id
+	dpo2.id_srgt as loader_id,
+	c2.DateKey as datetime_key_taken,
+	cmg2.TimeKey as time_key_taken,
+	c3.DateKey as datetime_key_close,
+	cmg3.TimeKey as time_key_close,
+	dlcp.srgt_id as prority_id,
+	lc.container_qty as container_qty,
+	lc.updated_at as updated_at,
+	now('Europe/Samara') AS update_etl
 from
 	silver_layer.loaders_calls lc
 left join gold_layer.dim_calendar dc ON
@@ -22,11 +32,16 @@ left join gold_layer.dim_loaders_workshops dlw ON
 lc.workshop_id = dlw.id
 left join gold_layer.dim_pa_oper dpo2 ON
 lc.loader_id = dpo2.tg_id
-; 
+left join gold_layer.dim_calendar c2 ON
+	c2.Date = toDate(formatDateTime(lc.taken_time, '%Y-%m-%d'))
+left join gold_layer.dim_calendar_minute_grain cmg2 ON
+	cmg2.`Time` = formatDateTime(lc.taken_time, '%H:%i')
+left join gold_layer.dim_calendar c3 ON
+	c3.`Date` = toDate(formatDateTime(lc.close_time, '%Y-%m-%d'))
+left join gold_layer.dim_calendar_minute_grain cmg3 ON
+	cmg3.`Time` = formatDateTime(lc.close_time, '%H:%i')
+left join gold_layer.dim_loaders_call_priorities dlcp ON
+lc.priority = dlcp.id;
 
 
-
-DESCRIBE TABLE silver_layer.pa_oper;
-
-
-#
+#TODO поля с предрассчитаныыми данными в слой витрин запилить (cdm-слой)
